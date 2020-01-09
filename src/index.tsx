bf9a8f0aab7c94c80acf50596d05fc8286e37135
Felipe Ospina <pipe.98.oh@gmail.com>
2020-01-09 00:53:59 -0500

    Better TypeScript integration Updateing tsconfig.json to modern Type-React app, and first time using React

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,14 @@
-console.log('Hello World!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default function App(): JSX.Element {
+  return (
+    <h1>
+      Hello!
+    </h1>
+  )
+}
+
+const root = document.getElementById('app-root')
+
+ReactDOM.render(<App />, root)
